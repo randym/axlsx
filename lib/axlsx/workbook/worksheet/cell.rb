3cd46c72882d3bb42bc46b67dbc12cbd805d0c4f
Randy Morgan <randym@reallyenglish.com>
2011-11-29 20:07:44 +0900

    trying to fix 'out of range' for time creation on travis

--- lib/axlsx/workbook/worksheet/cell.rb
+++ lib/axlsx/workbook/worksheet/cell.rb
@@ -169,8 +169,8 @@ module Axlsx
     def cast_value(v)
       if (@type == :time && v.is_a?(Time)) || (@type == :time && v.respond_to?(:to_time))
         v = v.respond_to?(:to_time) ? v.to_time : v
-        epoc = Workbook.date1904 ? Time.local(1904,1,1,0,0,0,0,v.zone) : Time.local(1900,1,1,0,0,0,0,v.zone)
-        ((v - epoc) /60.0/60.0/24.0).to_f
+        epoc = Workbook.date1904 ? Time.local(1904,1,1,0,0,0,0) : Time.local(1900,1,1,0,0,0,0)
+        ((v.localtime - epoc) /60.0/60.0/24.0).to_f
       elsif @type == :float
         v.to_f
       elsif @type == :integer
