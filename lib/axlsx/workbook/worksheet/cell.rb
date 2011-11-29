8de2f93355f7693060f7b821c00fa1fbf2b2e508
Randy Morgan <randym@reallyenglish.com>
2011-11-29 20:26:39 +0900

    always read the manual! Month is a string in the example on 1.8.7

--- lib/axlsx/workbook/worksheet/cell.rb
+++ lib/axlsx/workbook/worksheet/cell.rb
@@ -169,8 +169,8 @@ module Axlsx
     def cast_value(v)
       if (@type == :time && v.is_a?(Time)) || (@type == :time && v.respond_to?(:to_time))
         v = v.respond_to?(:to_time) ? v.to_time : v
-        epoc1900 = Time.local(1900, 1, 1)
-        epoc1904 = Time.local(1904,1, 1) 
+        epoc1900 = Time.local(1900, "jan", 1)
+        epoc1904 = Time.local(1904,"jan", 1) 
         epoc = Workbook.date1904 ? epoc1904 : epoc1900
         ((v.localtime - epoc) /60.0/60.0/24.0).to_f
       elsif @type == :float
