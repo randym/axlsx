af7309d057f184d4ba075e5b3108ca95c08de48e
Jonathan Tron <jonathan@tron.name>
2012-02-22 19:04:40 +0100

    Remove debugging in example.rb

--- examples/example.rb
+++ examples/example.rb
@@ -116,10 +116,8 @@
 ##Automatic cell types
 
      wb.add_worksheet(:name => "Automatic cell types") do |sheet|
-       t = Time.now
        sheet.add_row ["Date", "Time", "String", "Boolean", "Float", "Integer"]
-       sheet.add_row [Date.today, t, "value", true, 0.1, 1]
-       sheet.add_row ["", t.to_f, "", "", "", ""]
+       sheet.add_row [Date.today, Time.now, "value", true, 0.1, 1]
      end
 
 ##Merging Cells.
