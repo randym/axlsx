5b1860121aa20b83033792192617fbe6cf9b455b
Jurriaan Pruis <email@jurriaanpruis.nl>
2012-03-02 10:23:03 +0100

    epoc => epoch

--- lib/axlsx/workbook/worksheet/date_time_converter.rb
+++ lib/axlsx/workbook/worksheet/date_time_converter.rb
@@ -9,8 +9,8 @@ module Axlsx
     # @param [Date] date the date to be serialized
     # @return [Numeric]
     def self.date_to_serial(date)
-      epoc = Axlsx::Workbook::date1904 ? Date.new(1904) : Date.new(1899, 12, 30)
-      (date-epoc).to_f
+      epoch = Axlsx::Workbook::date1904 ? Date.new(1904) : Date.new(1899, 12, 30)
+      (date-epoch).to_f
     end
 
     # The time_to_serial methond converts a Time object its excel serialized form.
@@ -18,12 +18,12 @@ module Axlsx
     # @return [Numeric]
     def self.time_to_serial(time)
       # Using hardcoded offsets here as some operating systems will not except
-      # a 'negative' offset from the ruby epoc.
-      epoc1900 = -2209161600 # Time.utc(1899, 12, 30).to_i
-      epoc1904 = -2082844800 # Time.utc(1904, 1, 1).to_i
+      # a 'negative' offset from the ruby epoch.
+      epoch1900 = -2209161600 # Time.utc(1899, 12, 30).to_i
+      epoch1904 = -2082844800 # Time.utc(1904, 1, 1).to_i
       seconds_per_day = 86400 # 60*60*24
-      epoc = Axlsx::Workbook::date1904 ? epoc1904 : epoc1900
-      (time.to_f - epoc)/seconds_per_day
+      epoch = Axlsx::Workbook::date1904 ? epoch1904 : epoch1900
+      (time.to_f - epoch)/seconds_per_day
     end
   end
 end
