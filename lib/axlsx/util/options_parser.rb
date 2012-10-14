6d7ffaf10ee0568389854d41ad657a9e63c1703c
Randy Morgan <digital.ipseity@gmail.com>
2012-10-14 10:35:47 +0900

    Updated options parser to reject nil values

--- lib/axlsx/util/options_parser.rb
+++ lib/axlsx/util/options_parser.rb
@@ -1,8 +1,8 @@
 module Axlsx
   module OptionsParser
     def parse_options(options={})
-      options.each do |o|
-        self.send("#{o[0]}=", o[1]) if self.respond_to? "#{o[0]}="
+      options.each do |key, value|
+        self.send("#{key}=", value) if self.respond_to?("#{key}=") && value != nil
       end
     end
   end
