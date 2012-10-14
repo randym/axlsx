1c0563cf9e39b8fe3df011d6a24f61345b139e59
Randy Morgan <digital.ipseity@gmail.com>
2012-10-14 11:03:20 +0900

    refactored Cfvo to use options parser and serialized attributes

--- lib/axlsx/workbook/worksheet/cfvo.rb
+++ lib/axlsx/workbook/worksheet/cfvo.rb
@@ -8,23 +8,8 @@ module Axlsx
   #
   class Cfvo
 
-    # Type (ST_CfvoType)
-    # The type of this conditional formatting value object. options are num, percent, max, min, formula and percentile
-    # @return [Symbol]
-    attr_reader :type
-
-
-    # Type (xsd:boolean)
-    # For icon sets, determines whether this threshold value uses the greater than or equal to operator. 0 indicates 'greater than' is used instead of 'greater than or equal to'.
-    # The default value is true
-    # @return [Boolean]
-    attr_reader :gte
-
-
-    # Type (ST_Xstring)
-    # The value of the conditional formatting object
-    # This library will accept any value so long as it supports to_s
-    attr_reader :val
+    include Axlsx::OptionsParser
+    include Axlsx::SerailizedAttriubtes
 
     # Creates a new Cfvo object
     # @option options [Symbol] type The type of conditional formatting value object
@@ -32,10 +17,26 @@ module Axlsx
     # @option options [String] val The value of the conditional formatting object
     def initialize(options={})
       @gte = true
-      options.each do |o|
-        self.send("#{o[0]}=", o[1]) if self.respond_to? "#{o[0]}="
-      end
+      parse_options options
     end
+
+    serializable_attributes :type, :gte, :val
+
+    # Type (ST_CfvoType)
+    # The type of this conditional formatting value object. options are num, percent, max, min, formula and percentile
+    # @return [Symbol]
+    attr_reader :type
+
+    # Type (xsd:boolean)
+    # For icon sets, determines whether this threshold value uses the greater than or equal to operator. 0 indicates 'greater than' is used instead of 'greater than or equal to'.
+    # The default value is true
+    # @return [Boolean]
+    attr_reader :gte
+
+    # Type (ST_Xstring)
+    # The value of the conditional formatting object
+    # This library will accept any value so long as it supports to_s
+    attr_reader :val
 
     # @see type
     def type=(v); Axlsx::validate_conditional_formatting_value_object_type(v); @type = v end
@@ -54,9 +55,8 @@ module Axlsx
     # @return [String]
     def to_xml_string(str = '')
       str << '<cfvo '
-      str << instance_values.map { |key, value| '' << key << '="' << value.to_s << '"' }.join(' ')
+      serialized_attributes str
       str << ' />'
     end
-
   end
 end
