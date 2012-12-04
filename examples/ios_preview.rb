798e96feccea88648e64f1eb02707375f52cd50c
Randy Morgan <digital.ipseity@gmail.com>
2012-12-04 09:55:58 +0900

    Created ios preview example
    This shows that shared_strings are required. Formula parsing is still
    outstanding. https://github.com/randym/axlsx/issues/149

---
+++ examples/ios_preview.rb
@@ -0,0 +1,14 @@
+#!/usr/bin/env ruby -w -s
+# -*- coding: utf-8 -*-
+$LOAD_PATH.unshift "#{File.dirname(__FILE__)}/../lib"
+
+#```ruby
+require 'axlsx'
+
+p = Axlsx::Package.new
+p.use_shared_strings = true
+s = p.workbook.add_worksheet(:name => "Formula test")
+s.add_row [1, 2, 3]
+s.add_row %w(a b c)
+s.add_row ["=SUM(A1:C1)"]
+p.serialize "ios_preview.xlsx"
