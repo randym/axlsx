fb67e06a5f4087ebee347867c13aa85c7d8db56d
Randy Morgan <digital.ipseity@gmail.com>
2013-04-12 08:45:13 +0900

    Updated profile to use ruby-prof as we are in 2.0 for dev these days.

--- test/profile.rb
+++ test/profile.rb
@@ -7,20 +7,19 @@
 
 $:.unshift "#{File.dirname(__FILE__)}/../lib"
 require 'axlsx'
-require 'perftools'
-Axlsx.trust_input = true
+require 'ruby-prof'
 row = []
 # Taking worst case scenario of all string data
 input = (32..126).to_a.pack('U*').chars.to_a
 20.times { row << input.shuffle.join}
-times = 3000
 
-PerfTools::CpuProfiler.start("/tmp/axlsx") do
+profile = RubyProf.profile do
   p = Axlsx::Package.new
   p.workbook.add_worksheet do |sheet|
-    times.times do
+    30.times do
       sheet << row
     end
   end
-  p.serialize("example.xlsx")
 end
+printer = RubyProf::CallTreePrinter.new(profile)
+printer.print(File.new('axlsx.qcachegrind', 'w'))
