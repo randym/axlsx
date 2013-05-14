7caf2ea9dfe92fa418f40132ea3c94d3da109f2c
Randy Morgan <digital.ipseity@gmail.com>
2013-05-14 18:17:42 +0100

    correct shared strings table xml:space attribute parsing and schema

--- lib/axlsx/workbook/shared_strings_table.rb
+++ lib/axlsx/workbook/shared_strings_table.rb
@@ -49,7 +49,7 @@ module Axlsx
     def to_xml_string(str='')
       str << '<?xml version="1.0" encoding="UTF-8"?><sst xmlns="' << XML_NS << '"'
       str << ' count="' << @count.to_s << '" uniqueCount="' << unique_count.to_s << '"'
-      str << 'xml:space="' << xml_space.to_s << '>' << @shared_xml_string << '</sst>'
+      str << ' xml:space="' << xml_space.to_s << '">' << @shared_xml_string << '</sst>'
     end
 
     private
