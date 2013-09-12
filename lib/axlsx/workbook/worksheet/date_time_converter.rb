8cdeac65f6acfa4f63fe03f3060d77e7b7520375
Randy Morgan <digital.ipseity@gmail.com>
2013-09-12 23:16:19 +0900

    Add support for automatically adjusting serialized times and dates to account for the zone

--- lib/axlsx/workbook/worksheet/date_time_converter.rb
+++ lib/axlsx/workbook/worksheet/date_time_converter.rb
@@ -10,7 +10,8 @@ module Axlsx
     # @return [Numeric]
     def self.date_to_serial(date)
       epoch = Axlsx::Workbook::date1904 ? Date.new(1904) : Date.new(1899, 12, 30)
-      (date - epoch).to_f
+      offset_date = date.respond_to?(:utc_offset) ? date + date.utc_offset.seconds : date
+      (offset_date - epoch).to_f
     end
 
     # The time_to_serial methond converts a Time object its excel serialized form.
@@ -19,11 +20,11 @@ module Axlsx
     def self.time_to_serial(time)
       # Using hardcoded offsets here as some operating systems will not except
       # a 'negative' offset from the ruby epoch.
-      epoch1900 = -2209161600 # Time.utc(1899, 12, 30).to_i
-      epoch1904 = -2082844800 # Time.utc(1904, 1, 1).to_i
-      seconds_per_day = 86400 # 60*60*24
+      epoch1900 = -2209161600.0 # Time.utc(1899, 12, 30).to_i
+      epoch1904 = -2082844800.0 # Time.utc(1904, 1, 1).to_i
+      seconds_per_day = 86400.0 # 60*60*24
       epoch = Axlsx::Workbook::date1904 ? epoch1904 : epoch1900
-      (time.to_f - epoch)/seconds_per_day
+      (time.utc_offset + time.to_f - epoch)/seconds_per_day
     end
   end
 end
