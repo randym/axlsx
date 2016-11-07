90b69543ccad773ffff581dd929d94502e45cae9
Evan Hallmark <ehallmark@gttgrp.com>
2016-11-07 11:53:17 -0800

    Updated pivot_table#pivot_field_for to use the axisPage attr for page fields instead of axisCol

--- lib/axlsx/workbook/worksheet/pivot_table.rb
+++ lib/axlsx/workbook/worksheet/pivot_table.rb
@@ -257,7 +257,7 @@ module Axlsx
       elsif columns.include? cell_ref
         '<pivotField axis="axisCol" compact="0" outline="0" subtotalTop="0" showAll="0" includeNewItemsInFilter="1">' + '<items count="1"><item t="default"/></items>' + '</pivotField>'
       elsif pages.include? cell_ref
-        '<pivotField axis="axisCol" compact="0" outline="0" subtotalTop="0" showAll="0" includeNewItemsInFilter="1">' + '<items count="1"><item t="default"/></items>' + '</pivotField>'
+        '<pivotField axis="axisPage" compact="0" outline="0" subtotalTop="0" showAll="0" includeNewItemsInFilter="1">' + '<items count="1"><item t="default"/></items>' + '</pivotField>'
       elsif data_refs.include? cell_ref
         '<pivotField dataField="1" compact="0" outline="0" subtotalTop="0" showAll="0" includeNewItemsInFilter="1">' + '</pivotField>'
       else
